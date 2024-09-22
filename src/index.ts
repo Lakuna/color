3cc3d70587e01ba5814094e57ad57b993b8d7cbe
Travis Martin <tjmartin2003@gmail.com>
2024-09-22 03:09:28 -0500

    Remove useless color name dictionaries.

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,14 @@
 // Names
 export { default as bangNames } from "./names/bangNames.js";
-export { default as bigNames } from "./names/bigNames.js";
 export { default as cne2007Names } from "./names/cne2007Names.js";
-export { default as cneCore2007Names } from "./names/cneCore2007Names.js";
 export { default as crayolaNames } from "./names/crayolaNames.js";
-export { default as css3Names } from "./names/css3Names.js";
 export { default as getName, getNameStandard } from "./names/getName.js";
 export { default as hollaschNames } from "./names/hollaschNames.js";
-export { default as html4Names } from "./names/html4Names.js";
-export { default as mediumNames } from "./names/mediumNames.js";
-export { default as nbsAnthusNames } from "./names/nbsAnthusNames.js";
 export { default as nbsIsccNames } from "./names/nbsIsccNames.js";
-export { default as ns3Names } from "./names/ns3Names.js";
 export { default as ntcNames } from "./names/ntcNames.js";
-export { default as oldRgbNames } from "./names/oldRgbNames.js";
-export { default as pigmentsNames } from "./names/pigmentsNames.js";
-export { default as ravelingNames } from "./names/ravelingNames.js";
 export { default as resene2010Names } from "./names/resene2010Names.js";
-export { default as reseneNames } from "./names/reseneNames.js";
-export { default as smallNames } from "./names/smallNames.js";
-export { default as thomasNames } from "./names/thomasNames.js";
 export { default as x11Names } from "./names/x11Names.js";
 export { default as xkcdNames } from "./names/xkcdNames.js";
-// TODO: winsor-newton: https://people.csail.mit.edu/jaffer/Color/winsor-newton.pdf
-// TODO: fs595c: https://people.csail.mit.edu/jaffer/Color/fs595c.pdf
-// TODO: fs595c1: https://people.csail.mit.edu/jaffer/Color/fs595c1.pdf
-// TODO: saturate: https://people.csail.mit.edu/jaffer/Color/saturate.pdf
 
 // Types
 export type { default as Adobe } from "./types/Adobe.js";
--- test/names.js
+++ test/names.js
@@ -1,25 +1,12 @@
 import {
 	bangNames,
-	bigNames,
 	cne2007Names,
-	cneCore2007Names,
 	crayolaNames,
-	css3Names,
 	hexToRgb,
 	hollaschNames,
-	html4Names,
-	mediumNames,
-	nbsAnthusNames,
 	nbsIsccNames,
-	ns3Names,
 	ntcNames,
-	oldRgbNames,
-	pigmentsNames,
-	ravelingNames,
 	resene2010Names,
-	reseneNames,
-	smallNames,
-	thomasNames,
 	x11Names,
 	xkcdNames
 } from "../dist/index.js";
@@ -36,40 +23,16 @@ describe("getNameStandard", () => {
 		);
 	});
 
-	it('should return big name "seagreen3"', () => {
-		expect(getNameStandard(emerald, bigNames)).to.equal("seagreen3");
-	});
-
 	it('should return CNE 2007 name "algae"', () => {
 		expect(getNameStandard(emerald, cne2007Names)).to.equal("algae");
-	});
-
-	it('should return CNE core 2007 name "jade"', () => {
-		expect(getNameStandard(emerald, cneCore2007Names)).to.equal("jade");
 	});
 
 	it('should return Crayola name "fern"', () => {
 		expect(getNameStandard(emerald, crayolaNames)).to.equal("fern");
 	});
 
-	it('should return CSS3 name "mediumseagreen"', () => {
-		expect(getNameStandard(emerald, css3Names)).to.equal("mediumseagreen");
-	});
-
 	it('should return Hollasch name "emeraldgreen"', () => {
 		expect(getNameStandard(emerald, hollaschNames)).to.equal("emeraldgreen");
-	});
-
-	it('should return HTML4 name "lime"', () => {
-		expect(getNameStandard(emerald, html4Names)).to.equal("lime");
-	});
-
-	it('should return medium name "aquamarine"', () => {
-		expect(getNameStandard(emerald, mediumNames)).to.equal("aquamarine");
-	});
-
-	it('should return NBS Anthus name "brilliantgreen"', () => {
-		expect(getNameStandard(emerald, nbsAnthusNames)).to.equal("brilliantgreen");
 	});
 
 	it('should return NBS ISCC name "brilliantyellowishgreen"', () => {
@@ -78,40 +41,12 @@ describe("getNameStandard", () => {
 		);
 	});
 
-	it('should return NS3 name "mediumseagreen"', () => {
-		expect(getNameStandard(emerald, ns3Names)).to.equal("mediumseagreen");
-	});
-
 	it('should return Name That Color name "emerald"', () => {
 		expect(getNameStandard(emerald, ntcNames)).to.equal("emerald");
 	});
 
-	it('should return old RGB name "mediumaquamarine"', () => {
-		expect(getNameStandard(emerald, oldRgbNames)).to.equal("mediumaquamarine");
-	});
-
-	it('should return pigments name "malachite"', () => {
-		expect(getNameStandard(emerald, pigmentsNames)).to.equal("malachite");
-	});
-
-	it('should return Raveling name "seagreen3"', () => {
-		expect(getNameStandard(emerald, ravelingNames)).to.equal("seagreen3");
-	});
-
 	it('should return Resene 2010 name "deyork"', () => {
 		expect(getNameStandard(emerald, resene2010Names)).to.equal("deyork");
-	});
-
-	it('should return Resene name "deyork"', () => {
-		expect(getNameStandard(emerald, reseneNames)).to.equal("deyork");
-	});
-
-	it('should return small name "aquamarine"', () => {
-		expect(getNameStandard(emerald, smallNames)).to.equal("aquamarine");
-	});
-
-	it('should return Thomas name "forestgreen"', () => {
-		expect(getNameStandard(emerald, thomasNames)).to.equal("forestgreen");
 	});
 
 	it('should return X11 name "seagreen3"', () => {
