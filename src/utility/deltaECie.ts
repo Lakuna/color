251537f01bbf23961e35a588260dbce949716c1e
Travis Martin <tjmartin2003@gmail.com>
2024-10-03 01:42:41 -0500

    Clarify CIE 76 delta function.

--- src/utility/deltaECie.ts
+++ src/utility/deltaECie.ts
@@ -2,7 +2,7 @@ import type Lab from "../types/Lab.js";
 import type Luv from "../types/Luv.js";
 
 /**
- * Calculates the ΔE* CIE between two CIELAB values. Based on the EasyRGB pseudocode.
+ * Calculates the ΔE* CIE (CIE 1976) between two CIELAB values. Based on the EasyRGB pseudocode.
  * @param color0 - The first color.
  * @param color1 - The second color.
  * @returns The difference between the colors.
@@ -12,7 +12,7 @@ import type Luv from "../types/Luv.js";
 export default function deltaECie(color0: Lab, color1: Lab): number;
 
 /**
- * Calculates the ΔE* CIE between two CIELUV values. Based on the EasyRGB pseudocode.
+ * Calculates the ΔE* CIE (CIE 1976) between two CIELUV values. Based on the EasyRGB pseudocode.
  * @param color0 - The first color.
  * @param color1 - The second color.
  * @returns The difference between the colors.
@@ -38,9 +38,7 @@ export default function deltaECie(
 	// eslint-disable-next-line prefer-destructuring
 	const cieB2 = color1[2];
 
-	const deltaE = Math.sqrt(
-		(cieL1 - cieL2) ** 2 + (cieA1 - cieA2) ** 2 + (cieB1 - cieB2) ** 2
-	);
+	const deltaE = Math.hypot(cieL1 - cieL2, cieA1 - cieA2, cieB1 - cieB2);
 
 	return deltaE;
 }
