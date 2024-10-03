7f19fca91e294a922472d71e51cc50f5922e9f57
Travis Martin <tjmartin2003@gmail.com>
2024-10-03 06:21:59 -0500

    Minor documentation fix.

--- src/utility/hexToRgb.ts
+++ src/utility/hexToRgb.ts
@@ -3,7 +3,7 @@ import type Rgb from "../types/Rgb.js";
 /**
  * Split a hexadecimal value into sRGB values.
  * @param hex - The hexidecimal representation of the number (i.e. `0x50c878`).
- * @returns R, G, and B sRGB values (each in the range `[0x00,0xff]`), in that order.
+ * @returns An sRGB color.
  * @public
  */
 export default function hexToRgb(hex: number): Rgb {
