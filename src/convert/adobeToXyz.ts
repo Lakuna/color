11e9182769e18d00727908a8cd8ee1981b97f98c
Travis Martin <tjmartin2003@gmail.com>
2025-12-19 04:46:50 -0600

    Clarify `adobeToXyz`.

--- src/convert/adobeToXyz.ts
+++ src/convert/adobeToXyz.ts
@@ -1,8 +1,9 @@
 import type Adobe from "../types/Adobe.js";
 import type Xyz from "../types/Xyz.js";
 
-const gamma = 2.19921875;
-const f = 100 / 0xff ** gamma;
+const gamma = 0x233 / 0x100; // https://www.adobe.com/digitalimag/pdfs/AdobeRGB1998.pdf
+
+const f = 100 / 0xff ** gamma; // `(x / 0xff) ** gamma * 100)` is equivalent to `x ** gamma * f`.
 
 /**
  * Convert the given Adobe RGB color to a CIEXYZ color. Based on the EasyRGB pseudo-code.
@@ -11,9 +12,9 @@ const f = 100 / 0xff ** gamma;
  * @public
  */
 export default function adobeToXyz(color: Adobe): Xyz {
-	const i0 = color[0] ** gamma * f; // `(color[0] / 0xff) ** gamma * 100`
-	const i1 = color[1] ** gamma * f; // `(color[1] / 0xff) ** gamma * 100`
-	const i2 = color[2] ** gamma * f; // `(color[2] / 0xff) ** gamma * 100`
+	const i0 = color[0] ** gamma * f;
+	const i1 = color[1] ** gamma * f;
+	const i2 = color[2] ** gamma * f;
 
 	return [
 		i0 * 0.57667 + i1 * 0.18555 + i2 * 0.18819,
